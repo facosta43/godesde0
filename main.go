ecbb247277df676f752a8731b3bafed46e63528d
Franklin Acosta <franklinacosta@MacBook-Pro-de-Franklin.local>
2024-10-09 19:49:49 -0500

    Primer Commit

---
+++ main.go
@@ -0,0 +1,16 @@
+// Siempre va esta primera linea
+package main
+
+//Importaciones
+// import (
+// 	"fmt"
+// )
+import (
+	"github.com/franklinacosta/godesde0/variables"
+)
+
+// Funciones
+func main() {
+	// fmt.Println("Hola mundo")
+	variables.MuestroEnteros()
+}
